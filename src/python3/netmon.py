1233a7913d7c42f08dd712cbb13d10a4ad8a5cde
RegisDevOps <wmasters@regis.edu>
2019-02-17 20:26:53 -0700

    Create netmon.py

---
+++ src/python3/netmon.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# monitor_wifi.py
+import os
+import psutil
+import urllib3
+import subprocess
+
+from time import sleep
+from datetime import datetime
+
+SLEEP_TIME = 10 * 60
+
+def is_internet_on(method=2):
+    """Check if the Internet connection is on."""
+
+    if method == 2:
+        # http://stackoverflow.com/questions/3764291/checking-network-connection
+        try:
+            http = urllib3.PoolManager()
+            http.request('GET', 'http://www.google.com', timeout=1, retries=False)
+            return True
+        except urllib3.exceptions.NewConnectionError:
+            return False
+    else:
+        print('# warning: unknown method in is_internet_on()')
+
+
+def main():
+    while True:
+        if not is_internet_on():
+            print('# network is down')
+        else:
+            print('# network is up')
+            pass
+
+        print('# sleeping...')
+        sleep(SLEEP_TIME)
+
+
+#############################################################################
+
+if __name__ == "__main__":
+    main()
